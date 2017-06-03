c0383fb9366cf039e1913fd12042b39d29fd42bc
Jan Greger Hemb <jan.greger@gmail.com>
2017-06-04 01:05:06 +0200

    Initial

---
+++ main.js
@@ -0,0 +1,15 @@
+//https://stackoverflow.com/questions/2421911/what-is-the-purpose-of-wrapping-whole-javascript-files-in-anonymous-functions-li
+(function(){
+
+	//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+	"use strict"
+
+
+
+	document.addEventListener("DOMContentLoaded", function(event) {
+
+		console.log("h");
+
+	});
+
+})();
